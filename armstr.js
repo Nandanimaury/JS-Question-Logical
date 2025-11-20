2e8075b06d910d73251702eabb3f9c47e761045a
Nandanimaury <nandanimaurya24@navgurukul.org>
2025-11-20 22:57:45 +0530

    Refactor Armstrong number calculation logic
    Updated the Armstrong number calculation to use Math.floor for digit extraction.

--- armstr.js
+++ armstr.js
@@ -3,23 +3,23 @@ let count = 0;
 let temp = num;
 let sum = 0;
 let copy = num;
+// Count digits
 while (copy > 0) {
     count++;
-    copy = (copy - (copy % 10)) / 10;
+    copy = Math.floor(copy / 10);
 }
 temp = num;
+// Calculate armstrong sum
 while (temp > 0) {
-    count++;
-    let digit = temp % 10; 
+    let digit = temp % 10;
     let power = 1;
+
     for (let i = 0; i < count; i++) {
         power *= digit;
     }
-
     sum += power;
-    temp = (temp - digit) / 10; 
+    temp = Math.floor(temp / 10);
 }
-
 console.log(sum === num ? "Armstrong Number" : "Not an Armstrong Number");
 
 ////////////////////////////////////////////////////////////////////////////
@@ -41,10 +41,10 @@ console.log(sum === num ? "Armstrong Number" : "Not an Armstrong Number");
 //         sum += power;
 //         temp = (temp - digit) / 10; 
 //     }
-
 //     return sum === n; 
 // }
 // console.log(isArmstrongNumber(153));  
 // console.log(isArmstrongNumber(9474)); 
 // console.log(isArmstrongNumber(123));  
+
 
